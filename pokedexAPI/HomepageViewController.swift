99534b2794d16f2890969d1bb3b1b0e3f818be91
emmachlo <34774445+emmachlo@users.noreply.github.com>
2019-01-01 14:52:40 +0100

    Récupération des noms en Français (2eme bouton)

--- pokedexAPI/HomepageViewController.swift
+++ pokedexAPI/HomepageViewController.swift
@@ -21,7 +21,6 @@ class HomepageViewController: UIViewController {
     
     @IBOutlet var test2: UILabel!
     @IBOutlet var test: UITextField!
-    
     @IBAction func connectPokeAPI(_ sender: UIButton) {
         Alamofire.request("https://pokeapi.co/api/v2/pokemon/1/").responseJSON { (res) in
             guard let json = res.result.value as? [String: Any] else {
@@ -43,11 +42,37 @@ class HomepageViewController: UIViewController {
             }
             let pokemon = Pokemon(id: id, name: name)
             self.test2.text = "Nom : \(pokemon.name) #\(pokemon.id)"
+            
             let imageURL = URL(string: image)
             let imageData = try! Data(contentsOf: imageURL!)
             self.imageTest.image = UIImage(data: imageData)
             print(pokemon)
             print(image)
+        }
+    }
+    
+    @IBAction func pokefr(_ sender: UIButton) {
+        Alamofire.request("https://pokeapi.co/api/v2/pokemon-species/1/").responseJSON { (res) in
+            guard let json = res.result.value as? [String: Any] else {
+                return
+            }
+            guard let names = json["names"] as? [[String:Any]] else {
+                return
+            }
+            guard let fr = names[6]["name"] as? String else {
+                return
+            }
+            self.test.text = fr
+            print(fr)
+            /*
+            guard let name3 = json["name"] as? String else {
+                print("Could not get pokemon's gloglo from JSON")
+                return
+            }
+            let id = 3
+            let pokemon2 = Pokemon(id: id, name: name3)
+            self.test.text = pokemon2.name
+            print(pokemon2.name)*/
         }
     }
 }
